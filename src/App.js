f0963d8436eb1b8071c6d07bc309e780e5e77151
Jackie <actions@github.com>
2023-11-09 15:44:59 -0500

    Set Up Basics

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
 import Navbar from './component/navbar/Navbar';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Shop from './page/Shop';
 
 function App() {
   return (
     <div>
+      <BrowserRouter>
       <Navbar />
+      <Routes>
+        <Route path='/' element={<Shop}></Route>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/component/navbar/Navbar.jsx
+++ src/component/navbar/Navbar.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
+import cart_icon from '../assets/cart_icon.png'
 
 const Navbar = () => {
+
+  const [menu, setMenu] = useState("shop");  
+
   return (
     <div className='navbar'>
-      
+      <div className="nav-logo">
+        <h1>Bling Bristles</h1>
+      </div>
+      <ul className="nav-menu">
+        <li onClick={()=>{setMenu("shop")}}>Shop{menu==="shop"?<hr/>:<></>}</li>
+        <li onClick={()=>{setMenu("men")}}>Men{menu==="men"?<hr/>:<></>}</li>
+        <li onClick={()=>{setMenu("women")}}>Women{menu==="women"?<hr/>:<></>}</li>
+        <li onClick={()=>{setMenu("kids")}}>Kids{menu==="kids"?<hr/>:<></>}</li>
+      </ul>
+      <div className="nav-login-cart">
+        <button>Login</button>
+        <img src={cart_icon} alt="" />
+        <div className="nav-cart-count">0</div>
+      </div>
     </div>
   )
 }
