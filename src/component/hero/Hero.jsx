2753b0b5ed4eb8dfdda2ffcdf84842476b38fa12
Jackie <actions@github.com>
2023-11-14 18:14:05 -0500

    Hero Section

--- src/component/hero/Hero.jsx
+++ src/component/hero/Hero.jsx
@@ -1,41 +1,54 @@
 import React from 'react'
 import './Hero.css'
-import arrow_icon from '../assets/arrow.png'
-import collage_1 from '../assets/collage_1.png'
-import collage_2 from '../assets/collage_2.jpg'
-import collage_3 from '../assets/collage_3.jpg'
-import collage_4 from '../assets/collage_4.png'
-import finger_icon from '../assets/finger_icon.png'
+import new_icon from '../assets/new_icon.png'
+import star from '../assets/star_img.png'
 
 const Hero = () => {
   return (
-    <div className='hero'>
-      <div className="hero-left">
-        <h1>NEW<br />ARRIVALS</h1>
-        <h6>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.</h6>
-        <div className="hero-latest-btn-icon">
-          <div className="hero-latest-btn">
-            <div>Get Your Bling On</div>
-            <img src={arrow_icon} alt="" />
-          </div>
-          <img className='finger-icon' src={finger_icon} alt="" />
+    <div>
+      <div className='hero'>
+        <div className="hero-left">
+          <h1>SHOP NEW ARRIVALS</h1>
+          <button>SHOP NOW</button>
+          <img className='img-1' src={star} alt="" />
+          <img className='img-2' src={star} alt="" />
+          <img className='img-3' src={star} alt="" />
+        </div>
+        <div className="hero-right">
+          <img src={new_icon} alt="" />
         </div>
       </div>
-      <div className="hero-right">
-        <div className="collage-1">
-          <img src={collage_1} alt="" />
-        </div>
-        <div className="collage-2">
-          <img src={collage_2} alt="" />
-        </div>
-        <div className="collage-3">
-          <img src={collage_3} alt="" />
-        </div>
-        <div className="collage-4">
-          <img src={collage_4} alt="" />
+      <div class="scroll-container">
+        <div class="scroll-box">
+          <div class="scroll-content">
+            <p>✧ SUMMER SALE ✧</p>
+            <p>✧ SUMMER SALE ✧</p>
+            <p>✧ SUMMER SALE ✧</p>
+            <p>✧ SUMMER SALE ✧</p>
+            <p>✧ SUMMER SALE ✧</p>
+            <p>✧ SUMMER SALE ✧</p>
+            <p>✧ SUMMER SALE ✧</p>
+            <p>✧ SUMMER SALE ✧</p>
+            <p>✧ SUMMER SALE ✧</p>
+            <p>✧ SUMMER SALE ✧</p>
+            <p>✧ SUMMER SALE ✧</p>
+            <p>✧ SUMMER SALE ✧</p>
+            <p>✧ SUMMER SALE ✧</p>
+            <p>✧ SUMMER SALE ✧</p>
+            <p>✧ SUMMER SALE ✧</p>
+            <p>✧ SUMMER SALE ✧</p>
+            <p>✧ SUMMER SALE ✧</p>
+            <p>✧ SUMMER SALE ✧</p>
+            <p>✧ SUMMER SALE ✧</p>
+            <p>✧ SUMMER SALE ✧</p>
+            <p>✧ SUMMER SALE ✧</p>
+            <p>✧ SUMMER SALE ✧</p>
+            <p>✧ SUMMER SALE ✧</p>
+          </div>
         </div>
       </div>
     </div>
+
   )
 }
 
--- src/component/navbar/Navbar.jsx
+++ src/component/navbar/Navbar.jsx
@@ -9,20 +9,25 @@ const Navbar = () => {
   const [menu, setMenu] = useState("shop");  
 
   return (
-    <div className='navbar'>
-      <div className="nav-logo">
-        <Link style={{ textDecoration: 'none'}} to='/'><h1>Bling Bristles</h1></Link>
+    <div>
+      <div className="fixed-bar">
+        FREE SHIPPING ON ALL ORDERS OVER $200
       </div>
-      <ul className="nav-menu">
-        <li onClick={()=>{setMenu("shop")}}><Link style={{ textDecoration: 'none'}} to='/'>Shop</Link>{menu==="shop"?<hr/>:<></>}</li>
-        <li onClick={()=>{setMenu("men")}}><Link style={{ textDecoration: 'none'}} to='/men'>Men</Link>{menu==="men"?<hr/>:<></>}</li>
-        <li onClick={()=>{setMenu("women")}}><Link style={{ textDecoration: 'none'}} to='/women'>Women</Link>{menu==="women"?<hr/>:<></>}</li>
-        <li onClick={()=>{setMenu("kids")}}><Link style={{ textDecoration: 'none'}} to='/kids'>Kids</Link>{menu==="kids"?<hr/>:<></>}</li>
-      </ul>
-      <div className="nav-login-cart">
-        <Link style={{ textDecoration: 'none'}} to='/login'><button>Login<img src={arrow_icon} alt="" /></button></Link>
-        <Link style={{ textDecoration: 'none'}} to='/cart'><img className='cart-icon' src={cart_icon} alt="" /></Link>
-        <div className="nav-cart-count">0</div>
+      <div className='navbar'>
+        <div className="nav-logo">
+          <Link style={{ textDecoration: 'none'}} to='/'><h1>Bling Bristles</h1></Link>
+        </div>
+        <ul className="nav-menu">
+          <li onClick={()=>{setMenu("shop")}}><Link style={{ textDecoration: 'none'}} to='/'>Shop</Link>{menu==="shop"?<hr/>:<></>}</li>
+          <li onClick={()=>{setMenu("men")}}><Link style={{ textDecoration: 'none'}} to='/men'>Men</Link>{menu==="men"?<hr/>:<></>}</li>
+          <li onClick={()=>{setMenu("women")}}><Link style={{ textDecoration: 'none'}} to='/women'>Women</Link>{menu==="women"?<hr/>:<></>}</li>
+          <li onClick={()=>{setMenu("kids")}}><Link style={{ textDecoration: 'none'}} to='/kids'>Kids</Link>{menu==="kids"?<hr/>:<></>}</li>
+        </ul>
+        <div className="nav-login-cart">
+          <Link style={{ textDecoration: 'none'}} to='/login'><button>Login<img src={arrow_icon} alt="" /></button></Link>
+          <Link style={{ textDecoration: 'none'}} to='/cart'><img className='cart-icon' src={cart_icon} alt="" /></Link>
+          <div className="nav-cart-count">0</div>
+        </div>
       </div>
     </div>
   )
